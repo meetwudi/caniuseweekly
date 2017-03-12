0589918bf09d5953615cfbe33cdd984675271031
tjwudi <webmaster@leapoahead.com>
2017-03-12 03:01:36 -0700

    Fill in spec attrs

--- caniuseweekly/cspec.py
+++ caniuseweekly/cspec.py
@@ -1,6 +1,17 @@
 import attr
+import enum
 
 from caniuseweekly.browser import Browser
+
+@enum.unique
+class CSpecStatus():
+    LS = 'WHATWG Living Standard'
+    REC = 'W3C Recommendation'
+    PR = 'W3C Proposed Recommendation'
+    CR = 'W3C Candidate Recommendation'
+    WD = 'W3C Working Draft'
+    OTHER = 'Non-W3C, but reputable'
+    UNOFF = 'Unofficial, Editor\'s Draft or W3C "Note"'
 
 
 @attr.s
@@ -9,3 +20,6 @@ class CSpec():
     supported by a certain browser according to Caniuse.com.
     """
     browser = attr.ib(validator=attr.validators.instance_of(Browser))
+    description = attr.ib(validator=attr.validators.instance_of(str))
+    spec_url = attr.ib(validator=attr.validators.instance_of(str))
+    title = attr.ib(validator=attr.validators.instance_of(str))
