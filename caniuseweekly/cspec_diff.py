009b16b27f14c32a09435404b5fd81c63485a857
John Wu <webmaster@leapoahead.com>
2017-03-16 20:27:14 -0700

    Implement algorithm to calculate new stats

--- caniuseweekly/cspec_diff.py
+++ caniuseweekly/cspec_diff.py
@@ -3,27 +3,30 @@ import enum
 import attr
 
 from caniuseweekly.browser import Browser
+from caniuseweekly.cspec_types import CSpecVersionStat
 
 
 def calculate_stat_add(latest_spec, previous_spec):
     results = []
     for browser_code_friendly_name, latest_spec_stats in latest_spec.stats.items():  # noqa: E501
-        if browser_code_friendly_name not in previous_spec:
-            return
-
-        previous_spec_stats = previous_spec.stats
+        previous_spec_stats = previous_spec.stats[browser_code_friendly_name]
         new_versions = set(latest_spec_stats.keys()) - \
             set(previous_spec_stats.keys())
+        if not new_versions:
+            continue
         new_stats = {
             version: latest_spec_stats[version]
             for version in new_versions
         }
         browser = Browser.from_code_friendly_name(browser_code_friendly_name)
-        results.append(_CSpecDiffItem(
-            browser=browser,
-            diff_type=CSpecDiffType.STAT_ADD,
-            new_stats=new_stats,
-        ))
+        for new_stat in new_stats.items():
+            results.append(_CSpecDiffItem(
+                browser=browser,
+                diff_type=CSpecDiffType.STAT_ADD,
+                new_stat=CSpecVersionStat(
+                    version=new_stat[0], support=new_stat[1]),
+            ))
+    return results
 
 
 @enum.unique
@@ -35,7 +38,7 @@ class CSpecDiffType(enum.Enum):
 class _CSpecDiffItem():
     browser = attr.ib(validator=attr.validators.instance_of(Browser))
     diff_type = attr.ib(validator=attr.validators.instance_of(CSpecDiffType))
-    new_stats = attr.ib()
+    new_stat = attr.ib()
 
 
 @attr.s
