720336aca8bb1fe2c117a45e4339c635c339a667
tjwudi <webmaster@leapoahead.com>
2017-03-18 17:11:57 -0700

    Discard name parameter

--- caniuseweekly/appconfig.py
+++ caniuseweekly/appconfig.py
@@ -2,18 +2,16 @@ import functools
 import os
 
 
-def appconfig(name):
-    def decorator(config_getter):
-        override = None
-        prefixed_name = 'caniuseweekly_{}'.format(name)
+def appconfig(config_getter):
+    override = None
+    prefixed_name = 'caniuseweekly_{}'.format(config_getter.__name__)
 
-        if prefixed_name in os.environ:
-            # override from environment variable
-            override = os.environ[prefixed_name]
+    if prefixed_name in os.environ:
+        # override from environment variable
+        override = os.environ[prefixed_name]
 
-        @functools.wraps(config_getter)
-        def wrapper():
-            # return hard-coded config value if no override happens
-            return override if override else config_getter()
-        return wrapper
-    return decorator
+    @functools.wraps(config_getter)
+    def wrapper():
+        # return hard-coded config value if no override happens
+        return override if override else config_getter()
+    return wrapper
--- tests/appconfig_test.py
+++ tests/appconfig_test.py
@@ -5,14 +5,14 @@ from caniuseweekly.appconfig import appconfig
 
 def test_appconfig_return_env_value():
     with mock.patch.dict('caniuseweekly.appconfig.os.environ', {'caniuseweekly_test_config': 'cool'}):  # noqa: E501
-        @appconfig(name='test_config')
+        @appconfig
         def test_config():
             return 'not cool'
         assert test_config() == 'cool'
 
 
 def test_appconfig_return_hardcode_value():
-    @appconfig(name='test_config')
+    @appconfig
     def test_config():
         return 'cool'
 
